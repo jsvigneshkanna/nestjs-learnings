dc95bdb26a7853ce2d9cdc3f5e44820efc59931a
jsvigneshkanna <Vignesh@propertyguru.tech>
2023-07-12 10:47:07 +0530

    module updated

--- messages/src/messages/messages.module.ts
+++ messages/src/messages/messages.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { MessagesController } from './messages.controller';
 
 @Module({
-  controllers: [MessagesController]
+    controllers: [MessagesController]
 })
 export class MessagesModule {}
